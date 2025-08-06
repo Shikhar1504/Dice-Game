2bcf2768fe595ccb5ff0c48f1f4331e47a7e5911
Shikhar Sinha <shikharsinha117@gmail.com>
2025-08-06 23:52:46 +0530

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Dice-Game
+# Dice Game
+
+This is a simple and fun two-player dice game where the winner is determined by who rolls the higher number. The game is built with HTML, CSS, and JavaScript.
+
+## How to Play
+
+1.  **Open the game:** Open the `index.html` file in your web browser.
+2.  **Refresh the page:** Every time you refresh the page, the dice are rolled, and a winner is declared.
+
+## Features
+
+*   A two-player dice game.
+*   Random dice rolls for each player.
+*   Clear winner declaration.
+*   Simple and intuitive interface.
+
+## Technologies Used
+
+*   **HTML:** For the structure of the game.
+*   **CSS:** For styling the game.
+*   **JavaScript:** For the game logic and interactivity.
+
+## Setup and Usage
+
+To play the game, simply open the `index.html` file in your web browser.
+
+```bash
+git clone https://github.com/Shikhar1504/Dice-Game.git
+cd Dice-Game
+open index.html
+```
+
+## Author
+
+*   **Shikhar Sinha** - *Initial work* - [Shikhar1504](https://github.com/Shikhar1504)
--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-var randomNumber1=Math.random();
-randomNumber1=randomNumber1*6+1;
-randomNumber1=Math.floor(randomNumber1);
-var a="dice"+randomNumber1+".png";
-var b="images/"+a;
-var c=document.querySelectorAll("img")[0];
-c.setAttribute("src",b);
-var randomNumber2=Math.random();
-randomNumber2=randomNumber2*6+1;
-randomNumber2=Math.floor(randomNumber2);
-var a="dice"+randomNumber2+".png";
-var b="images/"+a;
-var c=document.querySelectorAll("img")[1];
-c.setAttribute("src",b);
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML="󠁧󠁢󠁷🚩 Player1 Wins!";
-}
-else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").innerHTML="Player2 Wins!󠁧󠁢🚩";
-}
-else{
-   document.querySelector("h1").innerHTML="Draw!";
-}
+document.querySelector(".btn").addEventListener("click", function () {
+  document.querySelector(".img1").classList.add("shake");
+  document.querySelector(".img2").classList.add("shake");
+
+  setTimeout(function () {
+    document.querySelector(".img1").classList.remove("shake");
+    document.querySelector(".img2").classList.remove("shake");
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage1 = "./images/dice" + randomNumber1 + ".png";
+    document.querySelector(".img1").setAttribute("src", randomDiceImage1);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var randomDiceImage2 = "./images/dice" + randomNumber2 + ".png";
+    document.querySelector(".img2").setAttribute("src", randomDiceImage2);
+
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    } else if (randomNumber2 > randomNumber1) {
+      document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    } else {
+      document.querySelector("h1").innerHTML = "Draw!";
+    }
+  }, 500);
+});
